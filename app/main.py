a7c17a98ab289b7ed1591d84b0a64588b1171c80
student <student@localhost>
2019-02-10 12:18:14 +0300

    циклы и последовательности

--- app/main.py
+++ app/main.py
@@ -1,42 +1,19 @@
-first_score = 7
-second_score = 10
-third_score = 10
+scores = [10, 10, 10]
 
 detractors_count = 0
 neutrals_count = 0
 promoters_count = 0
 
-if first_score < 7:
-    # detractors_count = detractors_count + 1
-    detractors_count += 1
+for score in scores:
+    if score < 7:
+        # detractors_count = detractors_count + 1
+        detractors_count += 1
 
-if 7 <= first_score <= 8:
-    neutrals_count += 1
+    if 7 <= score <= 8:
+        neutrals_count += 1
 
-if first_score > 8:
-    promoters_count += 1
+    if score > 8:
+        promoters_count += 1
 
-# как интерпретировать невалидные данные?
-
-if second_score < 7:
-    # detractors_count = detractors_count + 1
-    detractors_count += 1
-
-if 7 <= second_score <= 8:
-    neutrals_count += 1
-
-if second_score > 8:  # Ctrl + Shift + Alt + левой кнопкой мыши кликать (множественные курсоры)
-    promoters_count += 1
-
-if third_score < 7:
-    # detractors_count = detractors_count + 1
-    detractors_count += 1
-
-if 7 <= third_score <= 8:
-    neutrals_count += 1
-
-if third_score > 8:  # Ctrl + Shift + Alt + левой кнопкой мыши кликать (множественные курсоры)
-    promoters_count += 1
-
-result = round((promoters_count - detractors_count) / 3 * 100)
+result = round((promoters_count - detractors_count) / len(scores) * 100)
 print(result)
